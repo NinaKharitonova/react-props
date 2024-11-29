a65ccb816dcae0c9cb87ffd6e0d42b59105a3abb
NinaKharitonova <ninakharitonova95@email.com>
2024-11-29 15:46:13 +0100

    Formazione main iniziale

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,22 @@
+import Cards from "../../components/Cards/Cards";
+import posts from "../../data/posts";
+
 export default function () {
   return (
     <main>
       <section>
         <div className="container">
-          <div className="card">
-            <img src=" ../src/img/foto.webp" alt="" />
-            <div className="card-body">
-              <h4>Titolo del post</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-                ut laborum blanditiis officia eius cumque ad dignissimos, quo
-                amet et delectus eaque excepturi odit voluptatibus! Enim cumque
-                nisi voluptates consequuntur!
-              </p>
-              <button className="btn">Leggi di più</button>
-            </div>
+          <div className="row">
+            {posts.map((post) => (
+              <Cards
+                key={post.id}
+                title={post.title}
+                image={post.image}
+                content={post.content}
+                tags={post.tags}
+                published={post.published}
+              />
+            ))}
           </div>
         </div>
       </section>
